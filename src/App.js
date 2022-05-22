091111964b91092c65892749a1bf5c9a36aa7966
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-22 10:50:36 +0500

    Snake jsx file open and finishing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Snake from "./Snake";
 import "./index.css";
 class App extends React.Component {
+  state = {
+    snakeDots: [
+      [0, 0],
+      [2, 0],
+    ],
+  };
   render() {
-    return <div className="game-area"></div>;
+    return (
+      <div className="game-area">
+        <Snake snakeDots={this.state.snakeDots} />
+      </div> 
+    );
   }
 }
 export default App;
