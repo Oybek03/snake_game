2228261df44f0ed552ade4c34cadcd65eda6db5f
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-22 10:33:12 +0500

    game area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-
+import "./index.css";
 class App extends React.Component {
-render(){
-  return (
-    <div className="game-area"></div>
-  )
+  render() {
+    return <div className="game-area"></div>;
+  }
 }
-}
+export default App;
