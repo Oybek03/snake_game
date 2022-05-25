b2ddebc7cdf5d7563ef70b6d58d3a03dc35ef046
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-25 09:47:57 +0500

    tuzatish

--- src/App.js
+++ src/App.js
@@ -114,9 +114,34 @@ class App extends React.Component {
       });
     }
   }
+  onAlert() {
+    return (
+      <div class="ui basic modal">
+        <div class="ui icon header">
+          <i class="archive icon"></i>
+          Archive Old Messages
+        </div>
+        <div class="content">
+          <p>
+            Your inbox is getting full, would you like us to enable automatic
+            archiving of old messages?
+          </p>
+        </div>
+        <div class="actions">
+          <div class="ui red basic cancel inverted button">
+            <i class="remove icon"></i>
+            No
+          </div>
+          <div class="ui green ok inverted button">
+            <i class="checkmark icon"></i>
+            Yes
+          </div>
+        </div>
+      </div>
+    );
+  }
   onGameOver() {
-    alert(`Game over. Snake length is ${this.state.snakeDots.length}`);
-
+    alert(`O'yin tugadi. sizning ballingiz ${this.state.snakeDots.length}`);
     this.setState(initialState);
   }
   render() {
